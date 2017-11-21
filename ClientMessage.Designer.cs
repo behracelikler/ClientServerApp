860395977f0cbb6bc98ce749949208fb9431d034
behracelikler <beehraa@gmail.com>
2017-11-21 11:13:51 +0300

    ClientApplication

---
+++ ClientMessage.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace ClientApplication
+{
+    partial class Form_Client
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_Send = new System.Windows.Forms.Button();
+            this.textBox_Message = new System.Windows.Forms.TextBox();
+            this.textBox_ServerPort = new System.Windows.Forms.TextBox();
+            this.textBox_ServerIP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_Send
+            // 
+            this.button_Send.Location = new System.Drawing.Point(501, 46);
+            this.button_Send.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.button_Send.Name = "button_Send";
+            this.button_Send.Size = new System.Drawing.Size(124, 42);
+            this.button_Send.TabIndex = 0;
+            this.button_Send.Tag = "";
+            this.button_Send.Text = "Send";
+            this.button_Send.UseVisualStyleBackColor = true;
+            this.button_Send.Click += new System.EventHandler(this.button_Gonder_Click);
+            // 
+            // textBox_Message
+            // 
+            this.textBox_Message.Location = new System.Drawing.Point(121, 95);
+            this.textBox_Message.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_Message.Multiline = true;
+            this.textBox_Message.Name = "textBox_Message";
+            this.textBox_Message.Size = new System.Drawing.Size(503, 389);
+            this.textBox_Message.TabIndex = 1;
+            this.textBox_Message.Tag = "";
+            this.textBox_Message.Text = "Please Enter Message";
+            // 
+            // textBox_ServerPort
+            // 
+            this.textBox_ServerPort.Location = new System.Drawing.Point(121, 63);
+            this.textBox_ServerPort.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_ServerPort.Name = "textBox_ServerPort";
+            this.textBox_ServerPort.Size = new System.Drawing.Size(371, 22);
+            this.textBox_ServerPort.TabIndex = 2;
+            this.textBox_ServerPort.Tag = "";
+            this.textBox_ServerPort.Text = "Server Port";
+            // 
+            // textBox_ServerIP
+            // 
+            this.textBox_ServerIP.Location = new System.Drawing.Point(121, 31);
+            this.textBox_ServerIP.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.textBox_ServerIP.Name = "textBox_ServerIP";
+            this.textBox_ServerIP.Size = new System.Drawing.Size(371, 22);
+            this.textBox_ServerIP.TabIndex = 3;
+            this.textBox_ServerIP.Tag = "";
+            this.textBox_ServerIP.Text = "Server IP";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(37, 34);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Server IP :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(25, 66);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Server Port :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // Form_Client
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(667, 516);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_ServerIP);
+            this.Controls.Add(this.textBox_ServerPort);
+            this.Controls.Add(this.textBox_Message);
+            this.Controls.Add(this.button_Send);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form_Client";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Send Message Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_Send;
+        private System.Windows.Forms.TextBox textBox_Message;
+        private System.Windows.Forms.TextBox textBox_ServerPort;
+        private System.Windows.Forms.TextBox textBox_ServerIP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ ClientMessage.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using GlobalLibrary;
+
+namespace ClientApplication
+{
+    public partial class Form_Client : Form
+    {
+        Parametre param = new Parametre();
+
+        public Form_Client()
+        {
+            InitializeComponent();
+
+            param.KeepServerLog = true;
+
+            DataBinding();
+        }
+
+        private void DataBinding()
+        {
+            textBox_ServerIP.DataBindings.Add("Text", param, "ServerIP", false);
+            textBox_ServerPort.DataBindings.Add("Text", param, "ServerPort", false);
+            textBox_Message.DataBindings.Add("Text", param, "Message", false);
+
+            param.Message = "Please Enter Message";
+            //param.ServerIP = "192.168.16.15"; 
+            //param.ServerPort = 8081;
+        }
+
+        private void button_Gonder_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(param.Message))
+            {
+                MessageBox.Show("Please enter a message.");
+                return;
+            }
+
+            TCPConnection();
+        }
+
+        private void TCPConnection()
+        {
+            try
+            {
+                TcpClient client = new TcpClient(param.ServerIP, param.ServerPort);
+
+                NetworkStream nwStream = client.GetStream();
+
+                SendMessage(nwStream, client);
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                SqlOperations.SQLLog(param.KeepServerLog, "Client - ERROR", "", "", "", param.ServerIP, param.ServerPort, String.Concat("ERROR ", ex.Message));
+                throw;
+            }
+            finally
+            {
+                SqlOperations.CloseConnection(param.KeepServerLog);
+            }
+        }
+
+        private void SendMessage(NetworkStream nwStream, TcpClient client)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            byte[] bytesToSend = ASCIIEncoding.ASCII.GetBytes(param.Message);
+
+            SqlOperations.SQLLog(param.KeepServerLog, "Client", param.Message, "", "", param.ServerIP, param.ServerPort, "");
+            sb.AppendLine(param.Message);
+            sb.AppendLine(String.Concat("Sending:", param.Message));
+
+            if (nwStream.CanWrite)
+            {
+                nwStream.Write(bytesToSend, 0, bytesToSend.Length);
+            }
+            else
+            {
+                sb.AppendLine("Error: Don't write stream ");
+                SqlOperations.SQLLog(param.KeepServerLog, "Client - ERROR", "", "", "", param.ServerIP, param.ServerPort, "Error: Don't write stream");
+            }
+            byte[] bytesToRead = new byte[client.ReceiveBufferSize];
+            int bytesRead = nwStream.Read(bytesToRead, 0, client.ReceiveBufferSize);
+            param.DataReceived = Encoding.ASCII.GetString(bytesToRead, 0, bytesRead);
+            sb.AppendLine(String.Concat("Received : ", Encoding.ASCII.GetString(bytesToRead, 0, bytesRead)));
+
+            param.Message = sb.ToString();
+            SqlOperations.SQLLog(param.KeepServerLog, "Client", "", param.DataReceived, "", param.ServerIP, param.ServerPort, "");
+        }
+    }
+
+    public class Parametre : INotifyPropertyChanged
+    {
+        //KeepServerLog is true : SQL write log; false, don't log
+        private Boolean fKeepServerLog;
+        public Boolean KeepServerLog
+        {
+            get { return fKeepServerLog; }
+            set
+            {
+                if (fKeepServerLog == value)
+                    return;
+                fKeepServerLog = value;
+                NotifyPropertyChanged("KeepServerLog");
+            }
+        }
+
+        private String fMessage;
+        public String Message
+        {
+            get { return fMessage; }
+            set
+            {
+                if (fMessage == value)
+                    return;
+                fMessage = value;
+                NotifyPropertyChanged("Message");
+            }
+        }
+
+        private String fDataReceived;
+        public String DataReceived
+        {
+            get { return fDataReceived; }
+            set
+            {
+                if (fDataReceived == value)
+                    return;
+                fDataReceived = value;
+                NotifyPropertyChanged("DataReceived");
+            }
+        }
+
+        private String fServerIP;
+        public String ServerIP
+        {
+            get { return fServerIP; }
+            set
+            {
+                if (fServerIP == value)
+                    return;
+                fServerIP = value;
+                NotifyPropertyChanged("ServerIP");
+            }
+        }
+
+        private Int32 fServerPort;
+        public Int32 ServerPort
+        {
+            get { return fServerPort; }
+            set
+            {
+                if (fServerPort == value)
+                    return;
+                fServerPort = value;
+                NotifyPropertyChanged("ServerPort");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(String propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+
+}
